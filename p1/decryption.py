7bc6e5897f3c1e7101fa24635443fea446e1c79f
armanheydari <RmanHeydari2000@gmail.co>
2022-03-17 16:39:16 +0330

    affine decryption

--- p1/decryption.py
+++ p1/decryption.py
@@ -1,87 +1,64 @@
 import string
+import nltk
+from nltk.corpus import words
+import math
 
-
-def preprocess_cipher(cipher_text):
-    return cipher_text.replace(' ', '').replace('\n', '')
-
-
-def normalize_dictionary(d):
-    d = dict(sorted(d.items(), key=lambda item: item[1], reverse=True))
-
-    d_min = min(d.values())
-    d_max = max(d.values())
-    for k in d:
-        d[k] = (d[k] - d_min) / (d_max - d_min)
-    return d
+nltk.download('words')
 
 
 class DecryptText:
     def __init__(self, cipher_text):
-        self.cipher = cipher_text
+        self.cipher = cipher_text.replace(' ', '').replace('\n', '')
         self.uppercase_list = list(string.ascii_uppercase)
-        self.key = {}
+        self.key = (0, 0)
         self.plain = ''
-        self.cipher_frequency = {}
-        self.english_frequency = {
-            'E': 12000,
-            'T': 9000,
-            'A': 8000,
-            'I': 8000,
-            'N': 8000,
-            'O': 8000,
-            'S': 8000,
-            'H': 6400,
-            'R': 6200,
-            'D': 4400,
-            'L': 4000,
-            'U': 3400,
-            'C': 3000,
-            'M': 3000,
-            'F': 2500,
-            'W': 2000,
-            'Y': 2000,
-            'G': 1700,
-            'P': 1700,
-            'B': 1600,
-            'V': 1200,
-            'K': 800,
-            'Q': 500,
-            'J': 400,
-            'X': 400,
-            'Z': 200
-        }
+        self.words = []
+        self.english_words_set = set(words.words())
 
-    def find_frequencies(self):
-        for c in self.uppercase_list:
-            self.cipher_frequency[c] = 0
+    def find_plain_with_key(self, a, b):
+        self.plain = ''
         for c in self.cipher:
-            self.cipher_frequency[c] += 1
+            index = self.uppercase_list.index(c)
+            for i in range(30):
+                temp = ((index + i*26 - b)/a)
+                if temp.is_integer():
+                    index = int(temp)
+                    break
+            self.plain += self.uppercase_list[index]
 
-    def find_plain_with_key(self):
-        for c in self.cipher:
-            self.plain += self.key[c]
-        return self.plain
-
-    def find_key(self):
-        self.find_frequencies()
-        self.english_frequency = normalize_dictionary(self.english_frequency)
-        self.cipher_frequency = normalize_dictionary(self.cipher_frequency)
-
-        temp = list(self.english_frequency.keys())
-        for i, c in enumerate(self.cipher_frequency.keys()):
-            self.key[c] = temp[i]
+    def find_words(self, max_depth=15):
+        word_first_index = 0
+        words = []
+        while word_first_index < len(self.plain):
+            word_last_index = word_first_index
+            for j in range(max_depth):
+                if self.plain[word_first_index:word_first_index + j + 1].lower() in self.english_words_set:
+                    word_last_index = word_first_index + j + 1
+            words.append(self.plain[word_first_index:word_last_index])
+            word_first_index = word_last_index
+        return words
 
     def decrypt(self):
-        self.find_key()
-        self.find_plain_with_key()
-        return self.plain
+        print("decryption started...")
+        for a in range(1, 26):
+            if math.gcd(a, 26) == 1:
+                for b in range(1, 26):
+                    self.find_plain_with_key(a, b)
+                    words = self.find_words()
+                    if len(self.cipher) > 2 * len(words):
+                        self.words = words
+                        self.key = (a, b)
+                        break
+
+            if len(self.words) > 0:
+                print("done with a,b:", self.key)
+                break
 
 
 if __name__ == '__main__':
     input_text = open('sc_p1_input.txt').read()
-    preprocessed_text = preprocess_cipher(input_text)
-    decrypt_text = DecryptText(preprocessed_text)
-    plain_text = decrypt_text.decrypt()
-    print(plain_text)
+    decrypt_text = DecryptText(input_text)
+    decrypt_text.decrypt()
     with open('sc_p1_output.txt', 'w') as output_file:
-        output_file.write(plain_text)
+        output_file.write(' '.join(decrypt_text.words))
+    print("you can see the plain text in sc_p1_output.txt!")
